1784dc044c16f523a539bdef6ed1b8d4f7c385c9
Yuji Sakaguchi <yujisakaguchi@ucsb.edu>
2024-03-21 23:01:51 -0700

    initial

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_8hLPwfWvw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'tla44jhe24k0uh778ibatjdg1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://nmjpv8soya.execute-api.us-west-1.amazonaws.com/production' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
+    }
+};
